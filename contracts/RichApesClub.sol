390259e11b1f6b51d8a15180932cb4502abc93cd
Michał Bartczak <probartczak@gmail.com>
2022-02-18 19:08:07 +0100

    contract which work on kovan

--- contracts/RichApesClub.sol
+++ contracts/RichApesClub.sol
@@ -3,19 +3,27 @@ pragma solidity 0.8.11;
 
 import "./ERC721A.sol";
 import "./Ownable.sol";
-import "./PaymentSplitter.sol";
 import "./Counters.sol";
 
 interface IExternalContract {
     function balanceOf(address owner) external view returns (uint256);
 }
 
-contract RichApesClub is ERC721A, Ownable, PaymentSplitter {
+contract RichApesClub is ERC721A, Ownable {
     using Strings for uint256;
     using Counters for Counters.Counter;
 
+    uint256 public maxMintSupply = 10000;
+    uint256 public limitPerWallet = 30;
+
+    string public baseURI;
+
+    bool public publicState = true;
+
+    uint256 immutable price = 77700000000000000; //0.0777 ETH
+
     // interaction with external contract
-    address externalContractAddress; // hardcoded, from constructor or set by method?
+    address public externalContractAddress; // hardcoded, from constructor or set by method?
 
     function setExternalContractAddress(address contractAddress) external onlyOwner {
         externalContractAddress = contractAddress;
@@ -31,14 +39,19 @@ contract RichApesClub is ERC721A, Ownable, PaymentSplitter {
         require(_amount > 0, "zero amount");
         require(_amount <= limitPerWallet, "can't mint so much tokens"); // this is per tx or overall? if overall we need to change to `_amount <= limitPerWallet - balanceOf(msg.sender)`
         require(totalSupply() + _amount <= maxMintSupply, "max supply exceeded");
-
-        if ( externalBalanceOf(msg.sender) > 0 && balanceOf(msg.sender) == 0 ) {
-            require(msg.value >= price * (_amount - 1) , "value sent is not correct");
-        } else {
-            require(msg.value >= price * _amount , "value sent is not correct");
-        }
+        require(msg.value >= price * _amount , "value sent is not correct");
 
        _safeMint(_msgSender(), _amount);
+    }
+
+    function claim() external payable {
+        // add bool claimState and require?
+        require(externalBalanceOf(msg.sender) > 0, "nothing to claim");
+        require(externalBalanceOf(msg.sender) != balanceOf(msg.sender), "already claimed"); // this is weak, should we store addresses which claimed?
+        require(externalBalanceOf(msg.sender) <= limitPerWallet, "can't mint so much tokens"); // this is per tx or overall? if overall we need to change to `externalBalanceOf(msg.sender) <= limitPerWallet - balanceOf(msg.sender)`
+        require(totalSupply() + externalBalanceOf(msg.sender) <= maxMintSupply, "max supply exceeded");
+
+        _safeMint(_msgSender(), externalBalanceOf(msg.sender));
     }
 
     // new set baseURI method
@@ -49,25 +62,9 @@ contract RichApesClub is ERC721A, Ownable, PaymentSplitter {
     // ------------------------------------
     // rest
 
-    uint256 public maxMintSupply = 10000;
-    uint256 public limitPerWallet = 30;
-
-    string public baseURI;
-
-    bool public publicState = true;
-
-    uint256 immutable price = 100000000000000000; //0.1 ETH
-
-    uint256[] private _teamShares = [100];
-
-    address[] private _team = [
-        0x9530EcAaF1A01Ad6034e5aA6a36B06a6b8a103bf
-    ];
-
     constructor()
-        ERC721A("RichApesClub", "RAC", limitPerWallet, maxMintSupply)
-        PaymentSplitter(_team, _teamShares) {
-        _transferOwnership(_team[0]);
+        ERC721A("RichApesClub", "RAC", limitPerWallet, maxMintSupply) {
+        _transferOwnership(0x9530EcAaF1A01Ad6034e5aA6a36B06a6b8a103bf);
     }
 
     function enable() public onlyOwner {
